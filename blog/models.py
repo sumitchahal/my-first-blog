a028249f887e3b1434c0b53f972849f2f68f776d
sumit <sumitchahal111111@gmail.com>
2020-06-01 16:14:15 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
+class Post(models.Model): 
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -13,6 +13,6 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
+    
     def __str__(self):
         return self.title
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
-Function views
+Function views import: 
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
